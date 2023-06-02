93bf022bd627f48689f2af901ad6ec53e6ccc39e
ie-consuegra <ie.consuegra@gmail.com>
2023-06-01 22:17:08 -0500

    Add User registration API
    Receive a json with username and pass
    Save it to the DB
    Return an "ok"

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,13 +1,18 @@
+import axios from "axios";
 import { useState } from "react";
 
 export default function Register(){
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  async function register(ev) {
+    ev.preventDefault();
+    await axios.post('/register', { username, password });
+  }
   return (
     <div className="bg-blue-50 h-screen flex items-center">
       <div className="w-64 mx-auto">
         <div className="w-full text-center text-3xl text-slate-700 mb-10">Chat registration</div>
-        <form>
+        <form onSubmit={register}>
           <input type="text" value={username} onChange={ev => setUsername(ev.target.value)} placeholder="Username"className="block p-2 mb-2 border w-full rounded-sm"/>
 
           <input type="password" value={password} onChange={ev => setPassword(ev.target.value)} placeholder="Password" className="block p-2 mb-2 border w-full rounded-sm"/>
